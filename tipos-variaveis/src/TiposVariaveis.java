91c9049ba99a217ab470ef5a25182711ceabb040
llpupp <llpupp@hotmail.com>
2022-10-21 11:16:10 -0300

    variações tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMinimo = 2.50;
+        double salarioMinimo = 2500;
+        double salarioMinimo = 2500.33;
+        byte idade = 48;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 1392544679L;
+        float Pi = 3.14F;
+        double salario = 1254.30;
+
+    
     }
 }
