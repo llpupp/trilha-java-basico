961b12b8375538b095b62a171b2d06e7bbf6ecc4
llpupp <llpupp@hotmail.com>
2022-10-21 15:33:42 -0300

    Operador unário

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -21,10 +21,22 @@ public class Operadores {
     System.out.println(concatenacao);
 
     int numero = 5;
-    System.out.println(- numero);
-    numero = - numero;
+    System.out.println(- numero); // equivale a numero = -5
+    numero = - numero; //equivale a numero = -5
     System.out.println(numero);
-    numero = numero *-1;
+    numero = numero *-1; //equivale a numero = +5 (troca o numero = -5 *-1 = +5)
     System.out.println(numero);
+
+    int num = 5;
+    num = num + 2;
+    num++; //= num = num + 1
+    System.out.println(++ num); //equivale a numero = 9
+
+    System.out.println(num --); //equivale a numero = 9
+
+      boolean variavel = true;
+      System.out.println(!variavel); //equivale a boolean variavel = false
+
+      variavel = !variavel; // sendo negada aqui e mudando definitivo para false, senão volta à original
   }  
 }
