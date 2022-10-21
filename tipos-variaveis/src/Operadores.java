7a2a4e590fa6005603a410133a233d54b021ea2e
llpupp <llpupp@hotmail.com>
2022-10-21 14:31:59 -0300

    Operadores aritméticos + concatenação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -19,7 +19,12 @@ public class Operadores {
     
     concatenacao = "1"+(1+1+1);
     System.out.println(concatenacao);
-  
-    
+
+    int numero = 5;
+    System.out.println(- numero);
+    numero = - numero;
+    System.out.println(numero);
+    numero = numero *-1;
+    System.out.println(numero);
   }  
 }
